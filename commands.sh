5b0e42d1425096fd3422e774b25f137cf6353c9d
sabyradinov <sabyradinov.maksat@gmail.com>
2024-02-24 19:17:12 +0600

    init

---
+++ commands.sh
@@ -0,0 +1,13 @@
+#base sh scripts
+pwd # show me working directory
+cd /foldername # change directory `..` go to parent folder, `-` go to previous folder
+mkdir # create new folder
+ls # show file list
+touch # create empty file
+mv "file name" "new name|new place" # rename or replace file, with `-r` move folder
+cp "new file name" "source of copy" # create copy of file, with `-r` copy folder
+rm "file name" # remove file, with `-r` remove folder
+echo "text" > "file name" # rewrite text in file
+echo "text" >> "file name" # add text to file
+echo $PATH # show system folders path
+cat "file name" # write file text to stdout
